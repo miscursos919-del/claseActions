223fec121c25b0415cf2091ab0e7c9e3c555ed1f
miscursos919-del <miscursos919@gmail.com>
2026-04-28 21:12:53 -0300

    Add weather fetching functionality using OpenWeatherMap

---
+++ Program.cs
@@ -0,0 +1,22 @@
+//Program.cs
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+  static async Task Main (string[] args)
+  {
+    string apiKey = Enviroment.GetEnviromentVariable("OPENWEATHERMAP_APIKEY");
+    string ciudad ="Amsterdam";
+    string url = $"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={apiKey}&units=metric";
+
+    using HttpClient client = new HttpClient();
+    var response = await client.GetAsync(url);
+    var content = await response.Content.ReadAsStringAsync();
+
+    Console.Writeline($"Clima Actual en {ciudad}:");
+    Console.Writeline(content);
+  }
+}
+    
